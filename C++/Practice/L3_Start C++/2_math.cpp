ae0e656f32969a4259f22a4e4c98d6c2a47f9552
Prince Ojha <princeojha014@gmail.com>
2026-01-24 17:11:19 +0530

    first commit

---
+++ C++/Practice/L3_Start C++/2_math.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter 1st Number :";
+    cin>>a;
+    cout<<"Enter 2nd Number : ";
+    cin>>b;
+    cout<<a+b;
+    cout<<a-b;
+    cout<<a*b;
+    cout<<a/b;
+}
